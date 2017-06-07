97f2942a3bc4f067a7ec441734061084e90a1ea6
KarlaNavaM <karla.nm8@gmail.com>
2017-06-07 10:55:42 -0500

    Arreglé unos detalles de estilos de la primer versión e inserté gif en home.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
 function setTime () {
-    setTimeout(function(){window,location.href="views/home.html";}, 9000);
+    setTimeout(function(){window,location.href="home.html";}, 5000);
+}
+
+function cargarPagina() {
+    setTime();
 }
 
 
-$(document).ready(setTime);
+$(document).ready(cargarPagina);
