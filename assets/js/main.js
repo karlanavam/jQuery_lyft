fcb4b978c756f624d68d18edc730dd8ec54b4577
karlaNava <karla.nm8@gmail.com>
2017-06-11 18:45:30 -0500

    Hice maquetación con detalle en nav, agregué funcionalidad en validación de input

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,19 @@
+var inputTelefono = document.getElementById("icon_telephone");
+var btnInputTelefono = document.getElementById("btn-telNext");
+
+inputTelefono.addEventListener("click", validarFormTel);
+
 function setTime () {
     setTimeout(function(){window,location.href="home.html";}, 5000);
+}
+
+function validarFormTel() {
+	if (inputTelefono.value.length == 8) {
+		btnInputTelefono.disabled = true;
+	}
+	else {
+		btnInputTelefono.disabled = false;
+	}
 }
 
 function cargarPagina() {
@@ -7,4 +21,4 @@ function cargarPagina() {
 }
 
 
-$(document).ready(cargarPagina);
+
